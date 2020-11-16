7897cdfb25518dc5e26a9103d38fb754c69f9c80
drprasadkhandekar <s.nimbenni1@gmail.com>
2020-11-16 13:24:19 +0530

    First Functional Component Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 
+import React from 'react';
 import './App.css';
-import vehicle from './Vehicle/vehicle';
+import Vehicle from './Vehicle/vehicle';
 
-function App() {
-  return (
-    <div className="App">
-      <vehicle vehicleType="Car" Name="Altroz" Fuel="Petrol"/>
-    </div>
-  );
+class App extends React.Component  {
+  constructor(props){
+    super(props);
+this.state={
+  vehicle:[
+    {VehicleType:"Car", Name: "Altroz", Manufacturer:"Tata", FuelType:"petrol"},
+    {VehicleType:"Car", Name: "Nexon", Manufacturer:"Tata", FuelType:"Electric"},
+    {VehicleType:"Car", Name: "XUV", Manufacturer:"Mahindra", FuelType:"Disel"},
+    {VehicleType:"Car", Name: "Baleno", Manufacturer:"Maruti Suzuki", FuelType:"petrol"},
+  ]
+}
+  }
+   render(){
+     return(
+       <div>
+         <h1>List of Vehicle</h1> 
+         <Vehicle VehicleType={this.state.vehicles[0].VehicleType} Name={this.state.vehicles[0].Name} Manufacturer={this.state.vehicles[0].Manufacturer} FuelType={this.state.vehicles[0].FuelType}/>
+         <Vehicle VehicleType={this.state.vehicles[1].VehicleType} Name={this.state.vehicles[1].Name} Manufacturer={this.state.vehicles[1].Manufacturer} FuelType={this.state.vehicles[1].FuelType}/>
+         <Vehicle VehicleType={this.state.vehicles[2].VehicleType} Name={this.state.vehicles[2].Name} Manufacturer={this.state.vehicles[2].Manufacturer} FuelType={this.state.vehicles[2].FuelType}/>
+         <Vehicle VehicleType={this.state.vehicles[3].VehicleType} Name={this.state.vehicles[3].Name} Manufacturer={this.state.vehicles[3].Manufacturer} FuelType={this.state.vehicles[3].FuelType}/>
+         
+         </div>
+     );
+   };
 }
 
 export default App;
--- src/Vehicle/vehicle.js
+++ src/Vehicle/vehicle.js
@@ -1,13 +1,14 @@
 import './vehicle.css'
 const vehicle = props=> (
-    <div>
+    <div className='car'>
 <h1>
 vehicle :{props.vehicleType}
 </h1>
 
 <p>Name:{props.Name}</p>
-<p>Manifracturer:{props.Manifracturer}</p>
+<p>Manufacturer:{props.Manufacturer}</p>
 <p>Fuel:{props.Fuel}</p>
+
     </div>
 )
 export default vehicle;
